fbd1d4d8013e97fa287b77eeabdd5e599d8fffe9
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2024-09-24 15:11:22 +0200

    VideoListItem component: initial version

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,19 +1,17 @@
 <script setup>
 import videos from '../assets/videos.json'
-
-const toHHMMSS = (seconds) => {
-  return new Date(seconds * 1000).toISOString().slice(11, 19)
-}
+import VideoListItem from '@/components/VideoListItem.vue'
 </script>
 
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
     <div v-for="video in videos" :key="video.id" class="flex flex-col max-w-sm rounded shadow-lg">
-      <img class="w-full" :src="video.thumbnailUrl" />
-      <div class="flex flex-grow flex-col gap-2 p-2 text-left justify-between">
-        <div class="font-bold text-xl">{{ video.title }}</div>
-        <p class="text-gray-700 text-base">{{ toHHMMSS(video.duration) }}</p>
-      </div>
+      <video-list-item
+        :duration="video.duration"
+        :id="video.id"
+        :thumbnail-url="video.thumbnailUrl"
+        :title="video.title"
+      />
     </div>
   </div>
 </template>
---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  duration: { type: Number, required: true },
+  id: { type: String, required: true },
+  thumbnailUrl: { type: String, required: true },
+  title: { type: String, required: true }
+})
+
+const toHHMMSS = (seconds) => {
+  return new Date(seconds * 1000).toISOString().slice(11, 19)
+}
+</script>
+
+<template>
+  <img class="w-full" :src="props.thumbnailUrl" :alt="props.title" />
+  <div class="flex flex-grow flex-col gap-2 p-2 text-left justify-between">
+    <div class="font-bold text-xl">{{ props.title }}</div>
+    <p class="text-gray-700 text-base">{{ toHHMMSS(props.duration) }}</p>
+  </div>
+</template>

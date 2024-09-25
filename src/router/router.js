e220c253e2463bfc171e213ce48c65e09c76a915
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2024-09-25 17:35:59 +0200

    Apply Prettier format

--- src/router/router.js
+++ src/router/router.js
@@ -5,14 +5,14 @@ import WatchVideoView from '@/views/WatchVideo.vue'
 import PageNotFound from '@/views/PageNotFound.vue'
 
 const routes = [
-    { path: '/', name: 'Home', component: TheHomeView },
-    { path: '/watch/:id', name: 'WatchVideo', component: WatchVideoView },
-    { path: '/:catchAll(.*)*', name: 'NotFound', component: PageNotFound },
+  { path: '/', name: 'Home', component: TheHomeView },
+  { path: '/watch/:id', name: 'WatchVideo', component: WatchVideoView },
+  { path: '/:catchAll(.*)*', name: 'NotFound', component: PageNotFound }
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
 })
 
 export default router

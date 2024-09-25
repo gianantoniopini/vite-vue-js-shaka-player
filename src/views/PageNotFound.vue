17c885ab1cbf5c8425e9843cb9a4720ed3ad2175
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2024-09-25 17:29:51 +0200

    PageNotFound view: button as router-link

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -2,11 +2,8 @@
   <div class="flex flex-col items-center">
     <h1 class="font-bold text-9xl">404</h1>
     <h2 class="text-2xl">Page not found</h2>
-    <RouterLink
-      :to="{ name: 'Home' }"
-      class="py-2 px-2 md:px-4 rounded inline-block text-gray-600 bg-gray-300 mt-16"
-    >
-      Back to home
+    <RouterLink :to="{ name: 'Home' }" v-slot="{ href, navigate }" class="mt-16" tabindex="-1">
+      <button :href="href" @click="navigate">Back to home</button>
     </RouterLink>
   </div>
 </template>

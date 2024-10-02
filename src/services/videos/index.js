11a6fdc387d7d512c8c195e5607aa81934d4eef8
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2024-10-02 16:19:21 +0200

    Videos service: produce random delays, add comments

--- src/services/videos/index.js
+++ src/services/videos/index.js
@@ -1,14 +1,33 @@
+// The videos data is sourced from a local .json file.
+// In the future this data might come, instead, from a REST API.
 import videos from './data.json'
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+const getRandomInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+/**
+ * Produces a random delay within a given range
+ * @param {number} minDelayInMilliseconds The minimum delay in milliseconds
+ * @param {number} maxDelayInMilliseconds The maximum delay in milliseconds
+ * @return {Promise<unknown>} A promise
+ */
+const wait = (minDelayInMilliseconds, maxDelayInMilliseconds) => {
+  const delayInMilliseconds = getRandomInteger(minDelayInMilliseconds, maxDelayInMilliseconds)
+  return new Promise((resolve) => setTimeout(resolve, delayInMilliseconds))
+}
 
 const getAllVideos = async () => {
-  await sleep(1000)
+  // Simulate a delay (this is done just to test that the Suspense component works as expected)
+  await wait(200, 2000)
+
   return videos
 }
 
 const getVideoById = async (id) => {
-  await sleep(1000)
+  // Simulate a delay (this is done just to test that the Suspense component works as expected)
+  await wait(100, 1000)
+
   return videos.find((el) => {
     return el.id === id
   })

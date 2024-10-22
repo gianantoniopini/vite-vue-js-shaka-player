48cd2ceaf236fb5667cc95456981f5d90955e42e
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2024-10-22 16:29:31 +0200

    WatchVideo view: clean-up css classes

--- src/views/WatchVideo.vue
+++ src/views/WatchVideo.vue
@@ -16,28 +16,28 @@ const video = await getVideoById(route.params.id)
 </script>
 
 <template>
-  <div v-if="video" class="flex flex-col gap-5 max-w-xl">
+  <div v-if="video" class="flex flex-col gap-5 max-w-xl text-start ow-anywhere">
     <video-player
       :manifest-url="video.manifestUrl"
       :thumbnail-url="video.thumbnailUrl"
       @status-change="onVideoPlayerStatusChange"
     />
-    <h1 class="text-start text-2xl font-bold">{{ video.title }}</h1>
-    <span class="text-start text-lg overflow-wrap">License: {{ video.license }}</span>
-    <details class="text-start text-lg overflow-wrap">
+    <h1 class="text-2xl font-bold">{{ video.title }}</h1>
+    <span class="text-lg">License: {{ video.license }}</span>
+    <details class="text-lg">
       <summary>Details</summary>
       <p>
         Learn more
         <a :href="video.detailsUrl" target="_blank">about this video (opens in new tab)</a>
       </p>
     </details>
-    <span class="text-start text-base overflow-wrap">{{ videoPlayerStatus }}</span>
+    <span class="text-base">{{ videoPlayerStatus }}</span>
   </div>
   <div v-else class="w-full"><p>Video unavailable</p></div>
 </template>
 
 <style scoped>
-.overflow-wrap {
+.ow-anywhere {
   overflow-wrap: anywhere;
 }
 </style>

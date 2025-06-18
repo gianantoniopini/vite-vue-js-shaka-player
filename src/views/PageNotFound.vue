bc1d46e1d6645a8292e212be31dd94edca3b537b
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2025-06-18 14:31:24 +0200

    refactor(404): refactor PageNotFound view template

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -4,10 +4,12 @@ import BlankLayout from '@/layouts/BlankLayout.vue'
 
 <template>
   <BlankLayout>
-    <div class="flex flex-col items-center">
-      <h1 class="font-bold text-9xl">404</h1>
-      <h2 class="text-2xl">Page not found</h2>
-      <RouterLink v-slot="{ href, navigate }" :to="{ name: 'Home' }" class="mt-16" tabindex="-1">
+    <div class="flex flex-col gap-16">
+      <header>
+        <h1 class="font-bold text-9xl">404</h1>
+        <h2 class="text-2xl">Page not found</h2>
+      </header>
+      <RouterLink v-slot="{ href, navigate }" :to="{ name: 'Home' }" tabindex="-1">
         <button :href="href" @click="navigate">Back to home</button>
       </RouterLink>
     </div>

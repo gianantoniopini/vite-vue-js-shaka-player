0b7035c235129c5e821e9c6507a43b9fdd35fb5c
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2025-06-23 16:05:26 +0200

    feat(navbar): add support for dark mode

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -9,19 +9,16 @@ const toggleNav = () => {
 </script>
 
 <template>
-  <nav class="px-6 py-8 md:flex md:justify-between md:items-center bg-indigo-600">
+  <nav class="p-4 md:flex md:justify-between md:items-center bg-transparent dark:bg-gray-900">
     <div class="flex items-center justify-between">
       <RouterLink
         :to="{ name: 'Home' }"
-        class="text-xl font-bold text-gray-100 md:text-2xl hover:text-indigo-400"
-        >Logo
-      </RouterLink>
+        class="text-xl font-bold md:text-2xl text-black dark:text-white"
+        >Logo</RouterLink
+      >
       <!-- Mobile menu button -->
       <div class="flex md:hidden" @click="toggleNav">
-        <button
-          type="button"
-          class="text-gray-100 hover:text-gray-400 focus:outline-none focus:text-gray-400"
-        >
+        <button type="button" class="text-slate-500 dark:text-white">
           <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
             <path
               fill-rule="evenodd"
@@ -35,9 +32,9 @@ const toggleNav = () => {
     <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
     <ul
       :class="showMenu ? 'flex' : 'hidden'"
-      class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0"
+      class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0 text-black/60 dark:text-white"
     >
-      <li class="text-gray-100 hover:text-indigo-400">
+      <li>
         <RouterLink :to="{ name: 'Home' }">Home</RouterLink>
       </li>
     </ul>
